36448f38f0e57ea03a3c990a3a8bc36f504f0833
s28113 <s28113@pjwstk.edu.pl>
2024-05-11 20:35:45 +0200

    Changed variables names

--- Naive-Bayes-Program/src/main.py
+++ Naive-Bayes-Program/src/main.py
@@ -5,22 +5,22 @@ test_file = '/Users/pjotr/PycharmProjects/Naive-Bayes/Naive-Bayes-Program/res/ag
 
 def load_data(file):
     reader = pd.read_csv(file, header=None)
-    x = reader.iloc[:, 1:]  # Features
-    y = reader.iloc[:, 0]   # Labels
-    return x, y
+    attributes = reader.iloc[:, 1:]
+    labels = reader.iloc[:, 0]
+    return attributes, labels
 
-def calculate_prior(train_y):
-    label_count = train_y.value_counts()    # Count occurrences of each label
-    return label_count / len(train_y)       # Calculate probabilities
+def calculate_prior(train_labels):
+    label_count = train_labels.value_counts()    # Count occurrences of each label
+    return label_count / len(train_labels)       # Calculate probabilities
 
-def laplace_smoothing(label_data, train_x):
+def laplace_smoothing(label_data, train_attributes):
     cond_probabilities = {}
-    for col in train_x.columns:  # Iterate over features
+    for col in train_attributes.columns:  # Iterate over features
         value_counts = label_data[col].value_counts(normalize=True).to_dict() # Count occurrences of each value
-        total_values = len(label_data) + len(train_x[col].unique())  # Total number of values for Laplace smoothing
+        total_values = len(label_data) + len(train_attributes[col].unique())  # Total number of values for Laplace smoothing
 
         cond_probabilities[col] = {}
-        for value in train_x[col].unique(): # Iterate over unique values in feature
+        for value in train_attributes[col].unique(): # Iterate over unique values in feature
             count = value_counts.get(value, 0) # Get count of value or 0 if missing
             smoothed_count = count + 1 # Laplace method
             probability = smoothed_count / total_values
@@ -28,13 +28,13 @@ def laplace_smoothing(label_data, train_x):
 
     return cond_probabilities
 
-def fit_model(train_x, train_y):
-    unique_labels = train_y.unique() # Get unique labels in training data
-    prior_probabilities = calculate_prior(train_y) # Calculate prior probabilities
+def fit_model(train_attributes, train_labels):
+    unique_labels = train_labels.unique() # Get unique labels in training data
+    prior_probabilities = calculate_prior(train_labels) # Calculate prior probabilities
     cond_probabilities = {}
     for label in unique_labels: # Iterate over unique labels
-        label_data = train_x[train_y == label] # Filter data for current label
-        cond_probabilities[label] = laplace_smoothing(label_data, train_x) # Apply Laplace smoothing
+        label_data = train_attributes[train_labels == label] # Filter data for current label
+        cond_probabilities[label] = laplace_smoothing(label_data, train_attributes) # Apply Laplace smoothing
     return prior_probabilities, cond_probabilities
 
 def predict_instance(row, prior, cond_probabilities):
@@ -50,29 +50,28 @@ def predict_instance(row, prior, cond_probabilities):
             max_label = label
     return max_label
 
-def predict(x_test, test_y, prior, cond_probabilities):
+def predict(test_attributes, test_labels, prior, cond_probabilities):
     predictions = []
-    for i, row in x_test.iterrows(): # Iterate over test instances
+    for i, row in test_attributes.iterrows(): # Iterate over test instances
         max_label = predict_instance(row, prior, cond_probabilities) # Predict label for current instance
         predictions.append(max_label) # Append predicted label to list
-        true_label = test_y.iloc[i]  # Get true label
-        print(f"Prediction - {max_label}, True Label - {true_label}")  # Print prediction and true label
+        print("Predicted:", max_label, "Actual:", test_labels.iloc[i])
     return predictions
 
-def accuracy(y_true, y_pred):
-    correct = sum(1 for true, pred in zip(y_true, y_pred) if true == pred) # Count correct predictions
-    return correct / len(y_true)
+def accuracy(true_labels, predicted_labels):
+    correct = sum(1 for true, pred in zip(true_labels, predicted_labels) if true == pred) # Count correct predictions
+    return correct / len(true_labels)
 
-def precision(y_true, y_pred):
-    correct_p = sum(1 for i in range(len(y_true)) if y_true[i] == 'p' and y_pred[i] == 'p')  # Count true positives
-    wrong_p = sum(1 for i in range(len(y_true)) if y_true[i] == 'e' and y_pred[i] == 'p')  # Count false positives
+def precision(true_labels, predicted_labels):
+    correct_p = sum(1 for i in range(len(true_labels)) if true_labels[i] == 'p' and predicted_labels[i] == 'p')  # Count true positives
+    wrong_p = sum(1 for i in range(len(true_labels)) if true_labels[i] == 'e' and predicted_labels[i] == 'p')  # Count false positives
     if correct_p + wrong_p == 0:
         return 0
     return correct_p / (correct_p + wrong_p)
 
-def recall(y_true, y_pred):
-    correct_p = sum(1 for i in range(len(y_true)) if y_true[i] == 'p' and y_pred[i] == 'p')  # Count true positives
-    wrong_n = sum(1 for i in range(len(y_true)) if y_true[i] == 'p' and y_pred[i] == 'e')  # Count false negatives
+def recall(true_labels, predicted_labels):
+    correct_p = sum(1 for i in range(len(true_labels)) if true_labels[i] == 'p' and predicted_labels[i] == 'p')  # Count true positives
+    wrong_n = sum(1 for i in range(len(true_labels)) if true_labels[i] == 'p' and predicted_labels[i] == 'e')  # Count false negatives
     if correct_p + wrong_n == 0:
         return 0
     return correct_p / (correct_p + wrong_n)
@@ -104,8 +103,8 @@ while True:
     choice = int(input("Enter your choice: "))
     if choice == 1:
         print("Training Model...")
-        train_x, train_y = load_data(train_file)
-        prior, cond_probabilities = fit_model(train_x, train_y)
+        train_attributes, train_labels = load_data(train_file)
+        prior, cond_probabilities = fit_model(train_attributes, train_labels)
         isTrained = True
         print("Model trained successfully")
     if choice == 2:
@@ -114,9 +113,9 @@ while True:
             continue
         else:
             print("Testing Model...")
-            test_x, test_y = load_data(test_file)
-            predictions = predict(test_x, test_y, prior, cond_probabilities)
-            acc, prec, rec, f_meas = evaluate(test_y, predictions)
+            test_attributes, test_labels = load_data(test_file)
+            predictions = predict(test_attributes, test_labels, prior, cond_probabilities)
+            acc, prec, rec, f_meas = evaluate(test_labels, predictions)
 
             print("----------Model Info----------")
             print("Accuracy:", acc)
@@ -132,7 +131,7 @@ while True:
             print("Model not tested yet")
             continue
         else:
-            acc, prec, rec, f_meas = evaluate(test_y, predictions)
+            acc, prec, rec, f_meas = evaluate(test_labels, predictions)
             print("----------Model Info----------")
             print("Accuracy:", acc)
             print("Precision:", prec)
@@ -142,5 +141,3 @@ while True:
         print("Closing...")
         print("------------------------------")
         exit()
-
-
